179c3c7a278b2e106ef9a3d4002bd1d1296b1e42
Chris Castle <castle@heroku.com>
2017-06-18 11:39:37 -0700

    Add more migrations handling env conditional

---
+++ db/migrate/20170619232105_create_salesforce_line_item_again.rb
@@ -0,0 +1,30 @@
+class CreateSalesforceLineItemAgain < ActiveRecord::Migration
+  def up
+    return if ENV["DEPLOYMENT"] != "production"
+    HerokuConnect.change_schema("salesforce") do
+      # create_table :lineitem__c do |t|
+        # t.column :sfid, "varchar(18)", unique: true
+        # t.column :name, "varchar(80)"
+        add_column :lineitem__c, :price__c, :float
+        add_column :lineitem__c, :product_id__c, "varchar(18)"
+        add_column :lineitem__c, :product_id__r__spree_id__c, "varchar(14)"
+        add_column :lineitem__c, :order_id__c, "varchar(18)"
+        add_column :lineitem__c, :order_id__r__spree_id__c, "varchar(14)"
+        add_column :lineitem__c, :spree_id__c, "varchar(14)"
+      # end
+    end
+  end
+
+  def down
+    return if ENV["DEPLOYMENT"] != "production"
+    HerokuConnect.change_schema("salesforce") do
+      # execute "DROP TABLE lineitem__c"
+      remove_column :lineitem__c, :price__c
+      remove_column :lineitem__c, :product_id__c
+      remove_column :lineitem__c, :product_id__r__spree_id__c
+      remove_column :lineitem__c, :order_id__c
+      remove_column :lineitem__c, :order_id__r__spree_id__c
+      remove_column :lineitem__c, :spree_id__c
+    end
+  end
+end
